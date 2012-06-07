1f18401abf452ed96483dfd7b6850c99a40693b3
msh-openerp <msh@tinyerp.com>
2012-06-07 11:07:58 +0530

    [FIX]Fixed the issue of action, when there is no record selected from listview, so it should show the message that no record is selected, instead of do the action on 0th record in listview, as same behavior is available in version 6.1

--- addons/openerp/static/javascript/form.js
+++ addons/openerp/static/javascript/form.js
@@ -964,12 +964,8 @@ function do_action(src, context_menu) {
             new ListView('_terp_list').getSelectedRecords().join(',') +
             ']';
         if (eval(params['_terp_selection']).length == 0) {
-            var ids = eval(jQuery('#_terp_ids').val());
-            if (ids && ids.length > 0){
-                params['_terp_selection'] = '[' + ids[0] + ']';
-            } else {
-                error_display(_('You must select one or several records !'));
-            }
+            error_display(_('You must select one or several records !'));
+            return;
         }
         var id = eval(params['_terp_selection'])[0]
     } else {
